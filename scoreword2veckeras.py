4bcea0e163b38ffda8218163a2a6dd0a24bbceaf
Hirotaka Niitsuma <niitsuma@mub.biglobe.ne.jp>
2016-01-07 18:52:45 +0900

    score word sg

--- scoreword2veckeras.py
+++ scoreword2veckeras.py
@@ -3,16 +3,21 @@
 
 # Licensed under the GNU Affero General Public License, version 3 - http://www.gnu.org/licenses/agpl-3.0.html
 
+import sys
+import itertools
+
 from numpy import zeros, random, sum as np_sum, add as np_add, concatenate, \
     repeat as np_repeat, array, float32 as REAL, empty, ones, memmap as np_memmap, \
     sqrt, newaxis, ndarray, dot, vstack, dtype, divide as np_divide
 
-import gensim.models.word2vec 
+import gensim.models.word2vec
+import gensim.utils
 
 from six.moves import xrange, zip
 from six import string_types, integer_types, itervalues
 
-import sys
+
+
 import random
 
 import numpy as np
@@ -27,20 +32,25 @@ from keras.layers.embeddings import Embedding
 from keras.optimizers import SGD
 from keras.objectives import mse
 
+#class ScoredVocab(gensim.models.word2vec.Vocab):
+
+def word_score2scored_word(word,score):
+    return [word,score]
+
+def scored_word2word(scored_word):
+    return scored_word[0]
+def scored_word2score(scored_word):
+    return scored_word[1]
+
 
 def train_sg_pair(model, word, context_index, alpha, learn_vectors=True, learn_hidden=True,
                   context_vectors=None, context_locks=None):
-
     if word not in model.vocab:
         return
     predict_word = model.vocab[word]  # target word (NN output)
-    
     if model.hs:
         y=np.zeros((len(model.vocab)), dtype=REAL)
         x1=np.zeros((len(model.vocab)), dtype=REAL)
-        # for k,i in enumerate(predict_word.code):
-        #     y[predict_word.point[k]]=i
-        #     x1[predict_word.point[k]]=1
         x1[predict_word.point]=1
         y[predict_word.point]=predict_word.code
         x0=context_index
@@ -50,55 +60,60 @@ def train_sg_pair(model, word, context_index, alpha, learn_vectors=True, learn_h
 
     # if model.negative:
 
-def train_batch_sg(model, sentences, alpha, work=None,batch_size=100):
+def train_batch_score_sg(model, scored_word_sentences, alpha, work=None,batch_size=100):
     
     batch_count=0
 
     train_x0=[[0]]*batch_size
     train_x1=[[0]]*batch_size
-    train_y=[[0]]*batch_size
+    train_y0=[[0]]*batch_size
+    train_y1=[[0]]*batch_size
 
     while 1:
-
-        for sentence in sentences:
-            word_vocabs = [model.vocab[w] for w in sentence if w in model.vocab and
+        for scored_word_sentence in scored_word_sentences:
+            #sentence=[scored_word2word(scored_word) for scored_word in scored_word_sentence]
+            
+            word_vocabs = [[model.vocab[w],s] for [w,s] in scored_word_sentence if w in model.vocab and
                            model.vocab[w].sample_int > model.random.rand() * 2**32]
-            for pos, word in enumerate(word_vocabs):
+            for pos, scored_word in enumerate(word_vocabs):
                 reduced_window = model.random.randint(model.window)  # `b` in the original word2vec code
-
+                word=scored_word2word(scored_word)
                 # now go over all words from the (reduced) window, predicting each one in turn
                 start = max(0, pos - model.window + reduced_window)
-                for pos2, word2 in enumerate(word_vocabs[start:(pos + model.window + 1 - reduced_window)], start):
+                for pos2, scored_word2 in enumerate(word_vocabs[start:(pos + model.window + 1 - reduced_window)], start):
+                    word2=scored_word2word(scored_word2)
                     # don't train on the `word` itself
                     if pos2 != pos:
                         xy=train_sg_pair(model, model.index2word[word.index], word2.index, alpha)
                         if xy !=None:
-                            (x0,x1,y)=xy
+                            (x0,x1,y0)=xy
+                            y1=scored_word2score(scored_word)
                             train_x0[batch_count]=[x0]
                             train_x1[batch_count]=x1
-                            train_y[batch_count]=y
+                            train_y0[batch_count]=y0
+                            train_y1[batch_count]=y1
                             batch_count += 1
                             
                             if batch_count >= batch_size :
-                                yield { 'index':np.array(train_x0), 'point':np.array(train_x1), 'code':np.array(train_y)}
+                                yield { 'index':np.array(train_x0), 'point':np.array(train_x1), 'code':np.array(train_y0),'score':np.array(train_y1)}
                                 batch_count=0
 
-def build_keras_model_sg(index_size,vector_size,vocab_size,code_dim,model=None):
-    #vocab_size=len(model.vocab)
-    ## #index_size=vocab_size
-    #index_size=len(self.docvecs) 
-    #code_dim=vocab_size
-    #vector_size=model.vector_size
+def build_keras_model_score_word_sg(index_size,vector_size,vocab_size,code_dim,score_vector_size,model=None):
 
     kerasmodel = Graph()
     kerasmodel.add_input(name='point', input_shape=(code_dim,), dtype=REAL)
-    kerasmodel.add_input(name='index' , input_shape=(1,), dtype=int)
     kerasmodel.add_node(kerasmodel.inputs['point'],name='pointnode')
+    
+    kerasmodel.add_input(name='index' , input_shape=(1,), dtype=int)
+
     kerasmodel.add_node(Embedding(index_size, vector_size, input_length=1),name='embedding', input='index')
     kerasmodel.add_node(Flatten(),name='embedflatten',input='embedding')
     kerasmodel.add_node(Dense(code_dim, activation='sigmoid',b_constraint = keras.constraints.maxnorm(0)), name='sigmoid', input='embedflatten')
     kerasmodel.add_output(name='code',inputs=['sigmoid','pointnode'], merge_mode='mul')
-    kerasmodel.compile('rmsprop', {'code':'mse'})
+
+    kerasmodel.add_node(Dense(score_vector_size,b_constraint = keras.constraints.maxnorm(0)), name='scorenode', input='embedflatten')
+    kerasmodel.add_output(name='score',input='scorenode')
+    kerasmodel.compile('rmsprop', {'code':'mse','score':'mse'})
     return kerasmodel
 
   
@@ -185,51 +200,95 @@ def build_keras_model_cbow(index_size,vector_size,vocab_size,code_dim,model=None
     kerasmodel.compile('rmsprop', {'code':'mse'}) 
     return kerasmodel
 
-                            
 
-class Word2VecKeras(gensim.models.word2vec.Word2Vec):
 
-     def train(self, sentences, total_words=None, word_count=0, chunksize=100, total_examples=None, queue_factor=2, report_delay=1):
-        vocab_size=len(self.vocab)
-        #print 'Word2VecKerastrain'
+
+class  ScoreWord2VecKeras(gensim.models.word2vec.Word2Vec):
+
+    def scan_vocab(self, scored_word_sentences, progress_per=10000, trim_rule=None):
+        scored_word_sentences1,        scored_word_sentences2        =itertools.tee(scored_word_sentences)
+        sentences=(
+            [
+            #[scored_word2word(scored_word),scored_word2score(scored_word)]
+            scored_word2word(scored_word)
+            for scored_word in scored_word_sentence ]
+                   for scored_word_sentence in scored_word_sentences1)
+        super(ScoreWord2VecKeras, self).scan_vocab(sentences, progress_per, trim_rule)
+        score_vec0=scored_word2score(scored_word_sentences2.next()[0])
+        self.score_vector_size=len(score_vec0)
+
+        
+
+    def train(self, scored_word_sentences, total_words=None, word_count=0, chunksize=100, total_examples=None, queue_factor=2, report_delay=1):
+        vocab_size=len(self.vocab) 
+        #print 'ScoreWord2VecKeras.train'
+
         #batch_size=800 ##optimized 1G mem video card
         batch_size=800
-        samples_per_epoch=int(self.window*2*sum(map(len,sentences)))
+        samples_per_epoch=int(self.window*2*sum(map(len,scored_word_sentences)))
         #print 'samples_per_epoch',samples_per_epoch
         if self.sg:
-            self.kerasmodel=build_keras_model_sg(index_size=vocab_size,vector_size=self.vector_size,vocab_size=vocab_size,code_dim=vocab_size,model=self)
-            self.kerasmodel.fit_generator(train_batch_sg(self, sentences, self.alpha, work=None,batch_size=batch_size),samples_per_epoch=samples_per_epoch, nb_epoch=self.iter)
+            #print 'sg'
+            self.kerasmodel=build_keras_model_score_word_sg(index_size=vocab_size,vector_size=self.vector_size,vocab_size=vocab_size,code_dim=vocab_size,score_vector_size=self.score_vector_size ,model=self)
+            #tmpg=train_batch_score_sg(self, scored_word_sentences, self.alpha, work=None,batch_size=batch_size)
+            #print tmpg.next()
+            self.kerasmodel.fit_generator(train_batch_score_sg(self, scored_word_sentences, self.alpha, work=None,batch_size=batch_size),samples_per_epoch=samples_per_epoch, nb_epoch=self.iter)
             self.syn0=self.kerasmodel.nodes['embedding'].get_weights()[0]
-        else:
-            self.kerasmodel=build_keras_model_cbow(index_size=vocab_size,vector_size=self.vector_size,vocab_size=vocab_size,code_dim=vocab_size,model=self)
-            self.kerasmodel.fit_generator(train_batch_cbow(self, sentences, self.alpha, work=None,batch_size=batch_size),samples_per_epoch=samples_per_epoch, nb_epoch=self.iter)
-            self.syn0=self.kerasmodel.nodes['embedding'].get_weights()[0]
+        # else:
+        #     self.kerasmodel=build_keras_model_cbow(index_size=vocab_size,vector_size=self.vector_size,vocab_size=vocab_size,code_dim=vocab_size,model=self)
+        #     self.kerasmodel.fit_generator(train_batch_cbow(self, sentences, self.alpha, work=None,batch_size=batch_size),samples_per_epoch=samples_per_epoch, nb_epoch=self.iter)
+        #     self.syn0=self.kerasmodel.nodes['embedding'].get_weights()[0]
 
 
 
+
+
+
+class LineScoredWordSentence(object):
+    def __init__(self, source,score_fn, max_sentence_length=10000, limit=None):
+        self.source = source
+        self.score_fn=score_fn
+        self.max_sentence_length = max_sentence_length
+        self.limit = limit
+
+    def __iter__(self):
+        try:
+            self.source.seek(0)
+            for line in itertools.islice(self.source, self.limit):
+                line = gensim.utils.to_unicode(line).split()
+                i = 0
+                while i < len(line):
+                    yield [[w,self.score_fn(w)] for w in line[i : i + self.max_sentence_length]]
+                    i += self.max_sentence_length
+        except AttributeError:
+            with gensim.utils.smart_open(self.source) as fin:
+                for line in itertools.islice(fin, self.limit):
+                    line = gensim.utils.to_unicode(line).split()
+                    i = 0
+                    while i < len(line):
+                        yield  [[w,self.score_fn(w)] for w in line[i : i + self.max_sentence_length]]
+                        i += self.max_sentence_length
+       
+    
 
 if __name__ == "__main__":
 
     input_file = 'test.txt'
 
-    vsk = Word2VecKeras(gensim.models.word2vec.LineSentence(input_file),iter=100)
-    vs = gensim.models.word2vec.Word2Vec(gensim.models.word2vec.LineSentence(input_file))
-    print( vsk.most_similar('the', topn=8))
-    print( vs.most_similar('the', topn=8))
-
-    vck = Word2VecKeras(gensim.models.word2vec.LineSentence(input_file),iter=100,sg=0)
-    vc = gensim.models.word2vec.Word2Vec(gensim.models.word2vec.LineSentence(input_file),sg=0)
-    print( vck.most_similar('the', topn=8))
-    print( vc.most_similar('the', topn=8))
-
-
-    from nltk.corpus import brown, movie_reviews, treebank
-    print(brown.sents()[0])
+    def dummy_score_vec(word):
+        return [len(word)/1.0,ord(word[0])/200.0 ]
+        #return [len(word)/0.2 ]
+        
+    # sws=list(LineScoredWordSentence(input_file,dummy_score_vec))
+    # print sws[0]
+    # svk=ScoreWord2VecKeras( sws)
+    svk=ScoreWord2VecKeras( LineScoredWordSentence(input_file,dummy_score_vec),iter=100)
     
-    br = gensim.models.word2vec.Word2Vec(brown.sents())
-    brk = Word2VecKeras(brown.sents(),iter=10)
+    #print svk.score_vector_size
+    #model1=build_keras_model_score_word_sg(index_size=3,vector_size=2,vocab_size=2,code_dim=2,score_vector_size=2)
 
-    print( brk.most_similar('the', topn=8))
-    print( br.most_similar('the', topn=8))
+    print( svk.most_similar('the', topn=8))
+
+
 
     
