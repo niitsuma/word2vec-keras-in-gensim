cd9c836b49f3b3aa5e24404e0447edfb48215c9a
Hirotaka Niitsuma <niitsuma@mub.biglobe.ne.jp>
2016-01-17 00:22:59 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='word2veckeras',
+      version='0.0.1',
+      description='wordvec based Kearas gensim',
+      author='Hirotaka Niitsuma',
+      author_email='hirotaka.niitsuma@gmail.com',
+      url='hhttps://github.com/niitsuma/word2vec-keras-in-gensim',
+      download_url='httphttps://github.com/niitsuma/word2vec-keras-in-gensim/archive/master.zip',
+      license='GNU Affero General Public License, version 3 - http://www.gnu.org/licenses/agpl-3.0.html',
+      install_requires=['gensim' 'keras' 'theano', 'pyyaml', 'six'],
+      extras_require={
+          'h5py': ['h5py'],
+      },
+      packages=find_packages())
